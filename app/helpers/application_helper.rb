daa7b7ea68914239d27406458dde26b7d0431317
mohamed325 <mohamed.325@buckeyemail.osu.edu>
2018-08-13 19:57:21 -0400

    Implemented advanced login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	#it is goin to return full title on each pate
 	def full_title(page_title = '')
-		base_title = 'Ruby on Rails Tutorial Sample App'
+		base_title = "Abdikarim's Sample App"
 		if page_title.empty?
 			base_title
 		else
