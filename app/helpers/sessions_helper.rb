9898b4bad895cc28f4e2cd872e5d6ce014e5ed74
mohamed325 <mohamed.325@buckeyemail.osu.edu>
2018-08-19 10:36:11 -0400

    Add user posts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -49,6 +49,9 @@ module SessionsHelper
 		session.delete(:forwarding_url)
 	end
 
-
+    # Stores the URL trying to be accessed.
+     def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+     end
 	
 end
